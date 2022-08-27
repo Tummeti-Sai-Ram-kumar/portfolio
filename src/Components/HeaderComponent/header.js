53c3a3735afa5ccaf376fcc0d45aa7482dda0dfe
Tummeti-Sai-Ram-kumar <tummetisairamkumar1234@gmail.com>
2022-08-27 16:13:05 +0530

    sec commit

--- src/Components/HeaderComponent/header.js
+++ src/Components/HeaderComponent/header.js
@@ -1,28 +1,35 @@
+import { toHaveFocus } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
+import { useState } from "react";
 import './header.css'
 
 const Header = () => {
+    const [MenuToggle,setMenuToggle] = useState(1);
+
+
     return(
         <header class="header" id="home"> 
 
+            <input type="checkbox" class="menu-input" id="menu-input-id"></input>
+            <label class="menu-label" for="menu-input-id" onClick={() => setMenuToggle(!MenuToggle)}></label>
+            
             <div class="title">
                 <p>Tummeti Sai Ram Kumar</p>
             </div>
 
-            <nav class="nav-contents">
+            <nav class={MenuToggle ? "nav-contents" : "nav-contents hidden"}>
                 <ul>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#skills">Skills</a></li>
                     <li><a href="#projectsnav">Projects</a></li>
                     <li><a href="#message">Contact</a></li>
-                    
                 </ul>
             </nav>
         </header>
-
         
-
     )
 }
+
+
 
 export default Header
